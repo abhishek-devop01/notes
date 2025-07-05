3433a8481575bf96cdfdc788d1da16e2bd091f93
abhishek-devop01 <flightmode@60gmail.com>
2025-07-05 22:33:16 +0530

    completed

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app=express()
+app.use(express.json())
+
+let notes = []
+
+app.post('/notes', (req,res)=>{
+     console.log(req.body);
+     notes.push(req.body)
+     res.json({
+          message:"notes created"
+     })
+})
+app.get('/notes', (req,res)=>{
+     res.json(notes)
+})
+app.delete('/notes/:index', (req,res)=>{
+     const index = req.params.index
+     notes.splice(index,1)
+     res.json({
+          message:"notes deleted"
+     })
+})
+app.patch('/notes/:index', (req,res)=>{
+     const index = req.params.index
+     const {title} = req.body
+     notes[index].title=title
+     res.json({
+          message:"notes updated"
+     })
+})
+app.listen(3000,()=>{
+     console.log("server is running");     
+})
